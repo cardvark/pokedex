e62fca96d56549fc4ad15a40f9f1ca8675289e9c
James Wang <wang.james.j@gmail.com>
2026-01-09 15:41:11 -0800

    Refactored pokeapi.go to consolidate api calls into single GetResource generic function.

--- internal/pokeapi/pokeapi.go
+++ internal/pokeapi/pokeapi.go
@@ -2,6 +2,7 @@ package pokeapi
 
 import (
 	"encoding/json"
+	"io"
 	"net/http"
 
 	"github.com/cardvark/pokedex/internal/pokecache"
@@ -32,77 +33,36 @@ type locationAreaPokemonEncountersResp struct {
 	} `json:"pokemon_encounters"`
 }
 
-/*
+func GetResource[T any](url string) (T, error) {
+	var result T
 
-Order:
-- API call to get an decode the json response into a go struct
-- commandExplore in repl.go to call the GetLocationAreaPokemonResp function and process the resp struct.
-- update API call to cache response.
-
-
-*/
-
-func GetLocationAreaPokemonEncountersResp(url string) (locationAreaPokemonEncountersResp, error) {
 	entryBytes, ok := pokecache.MemCache.Get(url)
 	if ok {
-		var locAreaPokeResp locationAreaPokemonEncountersResp
-		err := json.Unmarshal(entryBytes, &locAreaPokeResp)
-		return locAreaPokeResp, err
+		err := json.Unmarshal(entryBytes, &result)
+		return result, err
 	}
 
 	res, err := http.Get(url)
 	if err != nil {
-		return locationAreaPokemonEncountersResp{}, err
+		return result, err
 	}
 	defer res.Body.Close()
 
-	var locAreaPokeResp locationAreaPokemonEncountersResp
-	decoder := json.NewDecoder(res.Body)
-
-	if err := decoder.Decode(&locAreaPokeResp); err != nil {
-		return locationAreaPokemonEncountersResp{}, err
-	}
-
-	resultBytes, err := json.Marshal(locAreaPokeResp)
+	dat, err := io.ReadAll(res.Body)
 	if err != nil {
-		return locationAreaPokemonEncountersResp{}, err
+		return result, err
 	}
 
-	pokecache.MemCache.Add(url, resultBytes)
+	pokecache.MemCache.Add(url, dat)
 
-	return locAreaPokeResp, nil
+	err = json.Unmarshal(dat, &result)
+	return result, err
+}
 
+func GetLocationAreaPokemonEncountersResp(url string) (locationAreaPokemonEncountersResp, error) {
+	return GetResource[locationAreaPokemonEncountersResp](url)
 }
 
 func GetLocationAreasResp(url string) (locationAreaResp, error) {
-	entryBytes, ok := pokecache.MemCache.Get(url)
-	if ok {
-		// fmt.Printf("Retrieving from cache! Raw value: %v\n", entryBytes)
-		var locAreasResp locationAreaResp
-		err := json.Unmarshal(entryBytes, &locAreasResp)
-		return locAreasResp, err
-	}
-
-	res, err := http.Get(url)
-	if err != nil {
-		return locationAreaResp{}, err
-	}
-	defer res.Body.Close()
-
-	var locAreasResp = locationAreaResp{}
-	decoder := json.NewDecoder(res.Body)
-
-	if err := decoder.Decode(&locAreasResp); err != nil {
-		return locationAreaResp{}, err
-	}
-
-	resultBytes, err := json.Marshal(locAreasResp)
-	if err != nil {
-		return locationAreaResp{}, err
-	}
-
-	pokecache.MemCache.Add(url, resultBytes)
-
-	return locAreasResp, nil
-
+	return GetResource[locationAreaResp](url)
 }
